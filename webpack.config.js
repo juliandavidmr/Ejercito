67d322c53cf5fa9441ccb61e6f9e149263c8ea0f
anlijudavid <anlijudavid@hotmail.com>
2016-10-13 14:09:52 -0500

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var rucksack = require('rucksack-css')
-var webpack = require('webpack')
-var path = require('path')
+var rucksack = require('rucksack-css');
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   context: path.join(__dirname, './client'),
@@ -21,34 +21,29 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    loaders: [
-      {
-        test: /\.html$/,
-        loader: 'file?name=[name].[ext]'
-      },
-      {
-        test: /\.css$/,
-        include: /client/,
-        loaders: [
-          'style-loader',
-          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        exclude: /client/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loaders: [
-          'react-hot',
-          'babel-loader'
-        ]
-      },
-    ],
+    loaders: [{
+      test: /\.html$/,
+      loader: 'file?name=[name].[ext]'
+    }, {
+      test: /\.css$/,
+      include: /client/,
+      loaders: [
+        'style-loader',
+        'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]',
+        'postcss-loader'
+      ]
+    }, {
+      test: /\.css$/,
+      exclude: /client/,
+      loader: 'style!css'
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loaders: [
+        'react-hot',
+        'babel-loader'
+      ]
+    }, ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -61,8 +56,11 @@ module.exports = {
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      }
     })
+    // new webpack.optimize.UglifyJsPlugin({minimize: true})
   ],
   devServer: {
     contentBase: './client',
